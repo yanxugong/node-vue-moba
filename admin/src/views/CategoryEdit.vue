f6a8646bb7e4eb2e1e4ccca5b591c89edbc55bdd
gongyanxu <745710524@qq.com>
2019-08-24 01:51:44 +0800

    add edit

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>新建分类</h1>
+    <h1>{{id ? '编辑' : '新建'}}分类 </h1>
     <el-form label-width="120px"
              @submit.native.prevent="save">
       <el-form-item label="名称">
@@ -16,19 +16,37 @@
 
 <script>
 export default {
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+  },
   data () {
     return {
       model: {}
     }
   },
+  created () {
+    this.id && this.fetch()
+  },
   methods: {
     async save () {
-      const res = this.$http.post('categories', this.model)
+      let res
+      if (this.id) {
+        res = await this.$http.put(`categories/${this.id}`, this.model)
+      } else {
+        res = await this.$http.post('categories', this.model)
+      }
       this.$router.push('/categories/list')
       this.$message({
         type: 'success',
         message: '保存成功'
       })
+    },
+    async fetch () {
+      const res = await this.$http.get(`categories/${this.id}`)
+      this.model = res.data
     }
   },
 }
