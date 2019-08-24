f9178f0b2ea56bdc49c35c9681601aa02449e268
gongyanxu <745710524@qq.com>
2019-08-24 21:23:48 +0800

    add upload img

--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -7,7 +7,16 @@
         <el-input v-model="model.name"></el-input>
       </el-form-item>
       <el-form-item label="图标">
-        <el-input v-model="model.icon"></el-input>
+        <el-upload class="avatar-uploader"
+                   :action="$http.defaults.baseURL + '/upload'"
+                   :show-file-list="false"
+                   :on-success="afterUpload">
+          <img v-if="model.icon"
+               :src="model.icon"
+               class="avatar">
+          <i v-else
+             class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
       </el-form-item>
       <el-form-item>
         <el-button type="primary"
@@ -34,6 +43,9 @@ export default {
     this.id && this.fetch()
   },
   methods: {
+    afterUpload (res) {
+      this.$set(this.model, 'icon', res.url)
+    },
     async save () {
       let res
       if (this.id) {
@@ -54,3 +66,32 @@ export default {
   },
 }
 </script>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -41,4 +41,12 @@ module.exports = app => {
     },
     router
   )
+
+  const multer = require('multer')
+  const upload = multer({ dest: __dirname + '/../../uploads' })
+  app.post('/admin/api/upload', upload.single('file'), async (req, res) => {
+    const file = req.file
+    file.url = `http://localhost:3000/uploads/${file.filename}`
+    res.send(file)
+  })
 }
