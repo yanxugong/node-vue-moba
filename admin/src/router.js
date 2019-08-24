1e495c1755efe86ef21bc66adba5d78cb4a90623
gongyanxu <745710524@qq.com>
2019-08-24 12:58:45 +0800

    add items

--- admin/src/router.js
+++ admin/src/router.js
@@ -4,6 +4,9 @@ import Main from './views/Main.vue'
 import CategoryEdit from './views/CategoryEdit.vue'
 import CategoryList from './views/CategoryList.vue'
 
+import ItemEdit from './views/ItemEdit.vue'
+import ItemList from './views/ItemList.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -15,7 +18,11 @@ export default new Router({
       children: [
         { path: '/categories/create', component: CategoryEdit },
         { path: '/categories/edit/:id', component: CategoryEdit, props: true },
-        { path: '/categories/list', component: CategoryList }
+        { path: '/categories/list', component: CategoryList },
+
+        { path: '/items/create', component: ItemEdit },
+        { path: '/items/edit/:id', component: ItemEdit, props: true },
+        { path: '/items/list', component: ItemList }
       ]
     }
   ]
--- admin/src/views/ItemList.vue
+++ admin/src/views/ItemList.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
-    <h1>分类列表</h1>
+    <h1>物品列表</h1>
     <el-table :data="items">
       <el-table-column prop="_id"
                        label="ID"
                        width="240">
       </el-table-column>
-      <el-table-column prop="parent.name"
-                       label="上级分类">
-      </el-table-column>
       <el-table-column prop="name"
-                       label="分类姓名">
+                       label="物品名称">
       </el-table-column>
       <el-table-column fixed="right"
                        label="操作"
@@ -18,7 +15,7 @@
         <template slot-scope="scope">
           <el-button type="text"
                      size="small"
-                     @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
+                     @click="$router.push(`/items/edit/${scope.row._id}`)">编辑</el-button>
           <el-button type="text"
                      size="small"
                      @click="remove(scope.row)">删除</el-button>
@@ -40,7 +37,7 @@ export default {
   },
   methods: {
     async fetch () {
-      const res = await this.$http.get('rest/categories')
+      const res = await this.$http.get('rest/items')
       this.items = res.data
     },
     async remove (row) {
@@ -49,7 +46,7 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(async () => {
-        const res = await this.$http.delete(`rest/categories/${row._id}`)
+        const res = await this.$http.delete(`rest/items/${row._id}`)
         this.$message({
           type: 'success',
           message: '删除成功!'
