19f9ed5ea57ca70f9ab1486b649749f551614e66
gongyanxu <745710524@qq.com>
2019-08-25 11:46:32 +0800

    add article

--- admin/src/router.js
+++ admin/src/router.js
@@ -7,6 +7,12 @@ import CategoryList from './views/CategoryList.vue'
 import ItemEdit from './views/ItemEdit.vue'
 import ItemList from './views/ItemList.vue'
 
+import HeroEdit from './views/HeroEdit.vue'
+import HeroList from './views/HeroList.vue'
+
+import ArticleEdit from './views/ArticleEdit.vue'
+import ArticleList from './views/ArticleList.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -22,7 +28,15 @@ export default new Router({
 
         { path: '/items/create', component: ItemEdit },
         { path: '/items/edit/:id', component: ItemEdit, props: true },
-        { path: '/items/list', component: ItemList }
+        { path: '/items/list', component: ItemList },
+
+        { path: '/heroes/create', component: HeroEdit },
+        { path: '/heroes/edit/:id', component: HeroEdit, props: true },
+        { path: '/heroes/list', component: HeroList },
+
+        { path: '/articles/create', component: ArticleEdit },
+        { path: '/articles/edit/:id', component: ArticleEdit, props: true },
+        { path: '/articles/list', component: ArticleList }
       ]
     }
   ]
