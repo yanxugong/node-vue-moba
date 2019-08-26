fe126191dafcf1dc2da0dcc2ed5ea73a5cfb918a
gongyanxu <745710524@qq.com>
2019-08-26 21:59:03 +0800

    add login verify

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,7 +1,34 @@
 import axios from 'axios'
+import Vue from 'vue'
 
 const http = axios.create({
   baseURL: 'http://localhost:3000/admin/api'
 })
+http.interceptors.request.use(
+  function(config) {
+    // Do something before request is sent
+    config.headers.Authorization = 'Bearer ' + localStorage.token
+    return config
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+http.interceptors.response.use(
+  res => {
+    return res
+  },
+  err => {
+    if (err.response.data.message) {
+      Vue.prototype.$message({
+        type: 'error',
+        message: err.response.data.message
+      })
+    }
+
+    return Promise.reject(err)
+  }
+)
 
 export default http
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,33 +1,52 @@
 module.exports = app => {
   const express = require('express')
+  const jwt = require('jsonwebtoken')
+  const AdminUser = require('../../models/AdminUser')
   const router = express.Router({
     mergeParams: true
   })
 
+  // 创建资源
   router.post('/', async (req, res) => {
     const model = await req.Model.create(req.body)
     res.send(model)
   })
+  // 更新资源
   router.put('/:id', async (req, res) => {
     const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
+  // 删除资源
   router.delete('/:id', async (req, res) => {
     await req.Model.findByIdAndDelete(req.params.id, req.body)
     res.send({
       success: true
     })
   })
-  router.get('/', async (req, res) => {
-    const queryOptions = {}
-    if (req.Model.modelName === 'Category') {
-      queryOptions.populate = 'parent'
+  // 资源列表
+  router.get(
+    '/',
+    async (req, res, next) => {
+      const token = String(req.headers.authorization || '')
+        .split(' ')
+        .pop()
+      const { id } = jwt.verify(token, app.get('secret'))
+      req.user = await AdminUser.findById(id)
+      console.log(req.user)
+      await next()
+    },
+    async (req, res) => {
+      const queryOptions = {}
+      if (req.Model.modelName === 'Category') {
+        queryOptions.populate = 'parent'
+      }
+      const items = await req.Model.find()
+        .setOptions(queryOptions)
+        .limit(10)
+      res.send(items)
     }
-    const items = await req.Model.find()
-      .setOptions(queryOptions)
-      .limit(10)
-    res.send(items)
-  })
+  )
+  // 资源详情
   router.get('/:id', async (req, res) => {
     const model = await req.Model.findById(req.params.id)
     res.send(model)
@@ -48,5 +67,26 @@ module.exports = app => {
     const file = req.file
     file.url = `http://localhost:3000/uploads/${file.filename}`
     res.send(file)
+  })
+
+  app.post('/admin/api/login', async (req, res) => {
+    const { username, password } = req.body
+    // 1.根据用户名找用户
+    const user = await AdminUser.findOne({ username }).select('+password')
+    if (!user) {
+      return res.status(422).send({
+        message: '用户不存在'
+      })
+    }
+    // 2.校验密吗
+    const isValid = require('bcrypt').compareSync(password, user.password)
+    if (!isValid) {
+      return res.status(422).send({
+        message: '密码错误'
+      })
+    }
+    // 3.返回token
+    const token = jwt.sign({ id: user._id }, app.get('secret'))
+    res.send({ token })
   })
 }
