c6653d6b0f5b15cb9fc2e2e7f74c0d49055a136f
gongyanxu <745710524@qq.com>
2019-08-27 22:00:59 +0800

    start code web

---
+++ web/src/views/Main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="topbar bg-black py-2 px-4">
+      <img src=""
+           alt="">
+    </div>
+    <div class="nav">
+      nav
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
